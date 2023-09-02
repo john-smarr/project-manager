d69517ace0732c11c4783ddde75cca8636c33762
John <john.smarr97@gmail.com>
2023-09-01 22:21:56 -0400

    added updating for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,8 +21,22 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def project_params
-      params.require(:project).permit(:name, :idea)
+      params.require(:project).permit(:name, :idea, :tools)
     end
 end
